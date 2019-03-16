4564080f882383f099f40afde78c53b33c3c8de5
Nilesh Sanyal <nil2take1@gmail.com>
2019-03-17 00:04:39 +0530

    Working on exam central functionality

--- app/routes/customer/customer.routes.js
+++ app/routes/customer/customer.routes.js
@@ -25,17 +25,16 @@ namedRouter.get('customer.register', '/register', function (req, res) {
 });
 
 namedRouter.get('customer.login', '/login', function (req, res) {
-
+  let stdLoginErrMessage = req.flash('stdLoginErrMessage')[0];
+  let stdLogoutSuccessMessage = req.flash('stdLogoutSuccessMessage')[0];
   res.render('customer/views/customer_login', 
         {
-            title: 'Login | EMS'
+            title: 'Login | EMS',
+            stdLoginErrMessage: stdLoginErrMessage,
+            stdLogoutSuccessMessage: stdLogoutSuccessMessage
         });
 
 });
-
-/* namedRouter.post('customer.login', '/login', function(req, res) {
-  console.log(req.body);
-}); */
 
 namedRouter.post('customer.login', '/login', passport.authenticate('customer-login', {
     
@@ -47,26 +46,28 @@ namedRouter.post('customer.login', '/login', passport.authenticate('customer-log
     res.redirect('/customer/dashboard');
   });
 
-//   namedRouter.get('customer.logout', '/logout', function(req, res){
-//     req.logout();
+  namedRouter.get('customer.logout', '/logout', function(req, res){
+    req.logout();
+    let stdLoginErrMessage = req.flash('stdLoginErrMessage')[0];
+    if (req.session) {
+        req.session.destroy(function(err) {
+          if(!err) {
+            res.render('customer/views/customer_login', 
+            {
+                title: 'Login | EMS',
+                stdLoginErrMessage: stdLoginErrMessage,
+                stdLogoutSuccessMessage: 'Log out successfully!',
+                /* adminCreateSuccessMessage: '',
+                logoutSuccessMessage: 'You are logged out',
+                unAuthorizedErrorMessage: '',
+                adminVerifySuccessMessage: '',
+                adminVerifyErrorMessage: '' */
+            });
+          } 
+        });
+      }
 
-//     if (req.session) {
-//         req.session.destroy(function(err) {
-//           if(!err) {
-//             res.render('customer/views/customer_login', 
-//             {
-//                 title: 'Login | EMS'
-//                 /* adminCreateSuccessMessage: '',
-//                 logoutSuccessMessage: 'You are logged out',
-//                 unAuthorizedErrorMessage: '',
-//                 adminVerifySuccessMessage: '',
-//                 adminVerifyErrorMessage: '' */
-//             });
-//           } 
-//         });
-//       }
-
-// });  
+});  
 
 namedRouter.get('customer.dashboard', '/dashboard', function (req, res) {
 
@@ -78,6 +79,7 @@ namedRouter.get('customer.dashboard', '/dashboard', function (req, res) {
 });
 
 namedRouter.get('customer.exam_central', '/exam-central', function (req, res) {
+  console.log(req.user);
   res.render('customer/views/student_exam_central', 
   {
       title: 'Exam Central | EMS'
--- config/stdpassport.js
+++ config/stdpassport.js
@@ -12,13 +12,13 @@ module.exports = (passport) => {
               User.findOne({
                 email: email,
                 contact_no: contact_no
-              }).exec((err, stduser) => {
+              }).exec((err, user) => {
                 if(err) {
-                    console.log('500 internal server error');
+                  console.log('500 internal server error');
                   return done(null, false, req.flash('stdLoginErrMessage', 'Something went wrong. Try again.'));
                 }
-                if(!stduser) {
-                    console.log('no student found');
+                if(!user) {
+                  console.log('no student found');
                   return done(null, false, req.flash('stdLoginErrMessage', 'No user found.'));  
                 }
                 else{
@@ -45,20 +45,20 @@ module.exports = (passport) => {
                    */     
 
 
-                    return done(null, stduser);  
+                    return done(null, user);  
                 }   
                     
               });
         })
       );
 
-      passport.serializeUser(function (stduser, done) {
-        done(null , stduser);
+      passport.serializeUser(function (user, done) {
+        done(null , user);
     });
     
     passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, stduser) {
-          done(err, stduser);
+        User.findById(id, function(err, user) {
+          done(err, user);
         });
       });
 
