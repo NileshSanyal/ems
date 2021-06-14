625bbe082cac209271369cd8b224dcd5361d827b
nileshsanyal2021 <nilesh.webideasolution@gmail.com>
2021-06-14 14:41:05 +0530

    testing

--- app/modules/admin/controllers/admin.controller.js
+++ app/modules/admin/controllers/admin.controller.js
@@ -15,7 +15,6 @@ exports.saveadmin = (req, res) =>{
             deffered.resolve({ "status": 200, data: result, "message": result  });    
 
     });
-    console.log('test');
     return deffered.promise;
 };
 
